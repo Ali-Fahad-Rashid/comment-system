8a26250625c03de03ab68bfd2ef51ff262bd8f2f
Ali-Fahad-Rashid <80278204+Ali-Fahad-Rashid@users.noreply.github.com>
2021-04-19 10:51:59 +0300

    Add files via upload

---
+++ admin/nav.php
@@ -0,0 +1,10 @@
+    <?php include "../header.php"; ?>
+    </br></br>
+    <div class="d-flex">
+    <div class="col-1"> </br></br>
+    <a href="index.php" class="btn">Admin</a>
+    <a href="posts.php" class="btn">Posts</a>
+    <a href="add_post.php" class="btn">Add Posts</a>
+    <a href="comments.php" class="btn">Comments</a>
+
+    </div>
